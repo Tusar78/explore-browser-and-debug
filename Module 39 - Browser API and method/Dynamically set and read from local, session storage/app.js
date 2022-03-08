f41811cae518b758c99c2cb75e1814289a9e30c5
Tusar <tusarahmed141@gmail.com>
2022-03-08 22:51:37 +0600

    Salary App

--- Module 39 - Browser API and method/Dynamically set and read from local, session storage/app.js
+++ Module 39 - Browser API and method/Dynamically set and read from local, session storage/app.js
@@ -1,8 +1,15 @@
+const displayLocalStorageProduct = () => {
+    const cart = getCart();
+    for (const name in cart) {
+        displayProduct(name)
+    }
+}
 const addItem = () => {
     const inputField = document.getElementById('product-name');
     const name = inputField.value;
     inputField.value = '';
     displayProduct(name);
+    productToCart(name)
 }
 
 const displayProduct = name => {
@@ -11,3 +18,32 @@ const displayProduct = name => {
     li.innerText = name;
     productContainer.appendChild(li);
 }
+
+const getCart = () => {
+    const cart = localStorage.getItem('cart');
+    let cartObj;
+    if (cart) {
+        cartObj = JSON.parse(cart);
+    } else {
+        cartObj = {};
+    }
+    return cartObj;
+}
+
+const productToCart = name => {
+    const cart = getCart();
+    if (cart[name]) {
+        cart[name] = cart[name] + 1;
+    } else {
+        cart[name] = 1;
+    }
+    const cartStringify = JSON.stringify(cart);
+    localStorage.setItem('cart', cartStringify);
+}
+
+const placeOrder = () => {
+    document.getElementById('product-container').textContent = '';
+    localStorage.removeItem('cart')
+}
+
+displayLocalStorageProduct()
