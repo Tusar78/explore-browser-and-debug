5ffa8083d341be74a983ef4481699a0d5818e66a
Tusar <tusarahmed141@gmail.com>
2022-03-12 23:48:35 +0600

    add image numbering

--- Mobule 43 - Bonus Module/Create JavaScript Slider/assets/js/app.js
+++ Mobule 43 - Bonus Module/Create JavaScript Slider/assets/js/app.js
@@ -3,7 +3,11 @@ const images = [
     './assets/images/img1.jpg',
     './assets/images/img2.jpg',
     './assets/images/img3.jpg',
-    './assets/images/img4.jpg'
+    './assets/images/img4.jpg',
+    './assets/images/img5.jpg',
+    './assets/images/img6.jpg',
+    './assets/images/img7.jpg',
+    './assets/images/img8.jpg'
 ]
 
 let imgIndex = 0;
