ebe6dca3054f8c03549a365ddc61e3efd5cc948b
Tusar <tusarahmed141@gmail.com>
2022-03-12 23:17:35 +0600

    Basic slider using javascript

---
+++ Mobule 43 - Bonus Module/Create JavaScript Slider/assets/js/app.js
@@ -0,0 +1,19 @@
+// Slider image container
+const images = [
+    './assets/images/img1.jpg',
+    './assets/images/img2.jpg',
+    './assets/images/img3.jpg',
+    './assets/images/img4.jpg'
+]
+
+let imgIndex = 0;
+const sliderImg = document.getElementById('slider-img');
+const imgNumbered = document.getElementById('img-number');
+setInterval(() => {
+    if (imgIndex >= images.length) {
+        imgIndex = 0
+    } 
+    sliderImg.setAttribute('src', images[imgIndex]);    
+    imgIndex++;
+    imgNumbered.innerText = `${imgIndex} / ${images.length}`    
+}, 1000);
